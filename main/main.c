8f493caf80d125739c49f8b10d0f2b20c7706907
aimannajjar <aiman.najjar@hurranet.com>
2024-11-24 08:26:00 -0500

    use managed components

--- main/main.c
+++ main/main.c
@@ -6,6 +6,12 @@
 
 #include "waveshare_rgb_lcd_port.h"
 
+#define lv_screen_active() lv_scr_act()
+#define lv_button_create(x) lv_btn_create(x)
+
+void lv_example_get_started_2(void);
+static void btn_event_cb(lv_event_t*);
+
 void app_main()
 {
     waveshare_esp32_s3_rgb_lcd_init(); // Initialize the Waveshare ESP32-S3 RGB LCD 
@@ -18,9 +24,42 @@ void app_main()
         // lv_demo_stress();
         // lv_demo_benchmark();
         // lv_demo_music();
-        lv_demo_widgets();
+
+        // lv_demo_widgets();
+
+        lv_example_get_started_2();
+        
         // example_lvgl_demo_ui();
         // Release the mutex
         lvgl_port_unlock();
     }
+}
+
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
+/**
+ * Create a button with a label and react on click event.
+ */
+void lv_example_get_started_2(void)
+{
+    lv_obj_t * btn = lv_button_create(lv_screen_active());     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+    lv_obj_center(label);
 }
