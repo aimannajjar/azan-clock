0cf55220d6ddccf23f69da48b9d84c2767314a20
aimannajjar <aiman.najjar@hurranet.com>
2024-12-19 11:22:05 -0500

    ui enhancements

--- main/ui/screens/ui_Main_Screen.c
+++ main/ui/screens/ui_Main_Screen.c
@@ -86,11 +86,11 @@ void ui_Main_Screen_screen_init(void)
     lv_obj_set_style_text_font(ui_Next_Prayer_Time, &lv_font_montserrat_30, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     ui_Current_Time = lv_label_create(ui_S1_Content_Panel);
-    lv_obj_set_width(ui_Current_Time, LV_SIZE_CONTENT);   /// 1
+    lv_obj_set_width(ui_Current_Time, 700);
     lv_obj_set_height(ui_Current_Time, LV_SIZE_CONTENT);    /// 1
-    lv_obj_set_x(ui_Current_Time, -25);
+    lv_obj_set_x(ui_Current_Time, 10);
     lv_obj_set_y(ui_Current_Time, -70);
-    lv_obj_set_align(ui_Current_Time, LV_ALIGN_CENTER);
+    lv_obj_set_align(ui_Current_Time, LV_ALIGN_LEFT_MID);
     lv_label_set_text(ui_Current_Time, "00:00\nAM");
     lv_obj_set_style_text_color(ui_Current_Time, lv_color_hex(0x9F4400), LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_text_opa(ui_Current_Time, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
